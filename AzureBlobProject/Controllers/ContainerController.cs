99befcaff1627813c409c0e8de1a6d968d0d3594
TomaszLenart <t.lenart234@gmail.com>
2026-02-13 00:52:47 +0100

    Init commit

---
+++ AzureBlobProject/Controllers/ContainerController.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+using AzureBlobProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobProject.Controllers
+{
+    public class ContainerController : Controller
+    {
+        private readonly IContainerService _containerService;
+
+        public ContainerController(IContainerService containerService)
+        {
+            _containerService = containerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allContainers = await _containerService.GetAllContainer();
+            return View(allContainers);
+        }
+
+        // GET: /Container/CreateNew
+        [HttpGet]
+        public IActionResult CreateNew()
+        {
+            // Returns the Create view (Views/Container/Create.cshtml)
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string containerName)
+        {
+            await _containerService.CreateContainer(containerName);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string containerName)
+        {
+            await _containerService.DeleteContainer(containerName);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,42 @@
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public ContainerService(BlobServiceClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+        public async Task CreateContainer(string containerName)
+        {
+            BlobContainerClient containerClient = _blobClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer);
+        }
+
+        public async Task DeleteContainer(string containerName)
+        {
+            BlobContainerClient containerClient = _blobClient.GetBlobContainerClient(containerName);
+            await containerClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> GetAllContainer()
+        {
+            List<string> containerNames = new();
+            await foreach (var container in _blobClient.GetBlobContainersAsync())
+            {
+                containerNames.Add(container.Name);
+            }
+            return containerNames;
+        }
+
+        public Task<List<string>> GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
