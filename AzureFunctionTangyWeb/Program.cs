b3bbe94d561dbbf2e7c87179a7cd7feca67026b7
TomaszLenart <t.lenart234@gmail.com>
2026-02-17 15:09:18 +0100

    AzureFunction project, post message to azure queue

---
+++ AzureFunctionTangyWeb/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient(); // Register HttpClient for dependency injection
+//builder.Services.AddHttpClient("TangyWebAPI", options =>
+//{
+//    options.BaseAddress = new Uri(builder.Configuration["TangyWebAPI:BaseUrl"]);
+//    // You can also set default headers here if needed
+//    // options.DefaultRequestHeaders.Add("Authorization", "Bearer YOUR_TOKEN");
+//});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
